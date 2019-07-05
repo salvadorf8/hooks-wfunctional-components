c4b1e8f632179051d8eb2603380bae088c94bce9
salvadorf8 <aztecsalvador@gmail.com>
2019-07-05 00:52:57 -0500

    SF - total excellent code reuse!!!!! with HOOKS!!

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,43 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-
-// putting all the logic in a function is an improvement because there is no tie to any component
-const useResources = (resource) => {
-    const [resources, setResources] = useState([]);
-
-    // lol, adding the resource object in the array was the solution as to why the fetchResource was not calling the service again.
-    // the second argument checks for a different value, if different, fetchResource will be called, if not different, useEffect will choose not 
-    // to call fetchResource again.
-    // RULE: you cannot invoke the above directly in the useEffect, however you can wrap the code in parenthesis, and invoke the function as shown below
-    useEffect(
-        () => {
-            (
-                async () => {
-                    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-                    setResources(response.data);
-                }
-            )(resource);
-        },
-        [resource]
-    );
-
-    return resources;
-};
+import useResources from './useResources';
 
 
 const ResourceList = ({ resource }) => {
     const resources = useResources(resource);
 
- 
-
-
     return (
         <ul>
-            {resources.map(record =>
-                <li key={record.id}>{record.title}</li>
-            )}
+            {resources.map( record => <li key={record.id}>{record.title}</li> )}
         </ul>
     );
 }
---
+++ src/components/useResources.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// putting all the logic in a function is an improvement because there is no tie to any component
+const useResources = (resource) => {
+    const [resources, setResources] = useState([]);
+
+    // lol, adding the resource object in the array was the solution as to why the fetchResource was not calling the service again.
+    // the second argument checks for a different value, if different, fetchResource will be called, if not different, useEffect will choose not 
+    // to call fetchResource again.
+    // RULE: you cannot invoke the above directly in the useEffect, however you can wrap the code in parenthesis, and invoke the function as shown below
+    useEffect(
+        () => {
+            (
+                async () => {
+                    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+
+                    setResources(response.data);
+                }
+            )(resource);
+        },
+        [resource]
+    );
+
+    return resources;
+};
+
+export default useResources;
